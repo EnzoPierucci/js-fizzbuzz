bcffee5203ed82b72e3a2bc982497878f918c936
EnzoPierucci <enzopierucci@gmail.com>
2024-01-09 14:41:17 +0100

    For + Bootstrap

---
+++ main.js
@@ -0,0 +1,27 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    
+    let container = document.createElement("div");
+    document.body.appendChild(container);
+
+    for (let i = 1; i <= 100; i++) {
+      let element = document.createElement("div");
+      element.classList.add("p-3", "m-3", "border", "rounded", "text-center");
+
+      if (i % 3 === 0 && i % 5 === 0) {
+        element.textContent = "FizzBuzz";
+        element.classList.add("bg-info", "text-white");
+      } else if (i % 3 === 0) {
+        element.textContent = "Fizz";
+        element.classList.add("bg-warning");
+      } else if (i % 5 === 0) {
+        element.textContent = "Buzz";
+        element.classList.add("bg-primary", "text-white");
+      } else {
+        element.textContent = i;
+        element.classList.add("bg-light");
+      }
+
+      container.appendChild(element);
+    }
+  });
